d1a5c8ac427f5a5962de029d7b3a9ebad92938b1
hayderAbdulsahib <abdulsahib.hayder@gmail.com>
2022-04-02 01:48:24 +0300

    adding files

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("hi");
+
+const h1 = document.createElement("input");
+
+const image = document.createElement("img");
+
+const cardDiv = document.querySelector(".cards-container");
+
+// cardDiv.append(h1);
+
+// cardDiv.append(image);
+
+image.src = "./vue.jpg";
+
+image.name = "hayyyy";
